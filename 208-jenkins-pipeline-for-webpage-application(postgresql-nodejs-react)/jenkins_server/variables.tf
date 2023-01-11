75094a130b3f841964dc2d33e8b22c53dfa572ab
ilgea <zubeyirozkaya@gmail.com>
2023-01-11 16:35:56 +0300

    added Jenkins project

---
+++ 208-jenkins-pipeline-for-webpage-application(postgresql-nodejs-react)/jenkins_server/variables.tf
@@ -0,0 +1,19 @@
+variable "mykey" {
+  default = "firstkey"
+}
+variable "myami" {
+  default = "ami-026b57f3c383c2eec"
+}
+variable "instancetype" {
+  default = "t3a.small"
+}
+variable "tag" {
+  default = "Jenkins_Server"
+}
+variable "jenkins-sg" {
+  default = "jenkins-server-sec-gr-208"
+}
+
+variable "user" {
+  default = "ilgea"
+}
